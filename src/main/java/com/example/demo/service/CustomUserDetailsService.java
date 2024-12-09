66331871c527667db54b04ec6a844ccaccab35f3
RltCodes <lalahtahiana@gmail.com>
2024-12-09 21:34:36 +0300

    fix bug login

---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+	private final UserRepository userRepository;
+	
+	public CustomUserDetailsService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user==null) {
+			throw new UsernameNotFoundException("Utilisateur non trouvé");
+		}
+		
+		return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream().map(Role::getName).toArray(String[]::new))
+                .build();
+				
+	}
+}
